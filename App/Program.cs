fa757aaada29b45c820ad9ff9763f2079634855c
Kaisu <kaisu.suurniemi@edu.karelia.fi>
2024-10-29 14:37:16 +0200

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿using AppLogger;
+using System;
+using static AppLogger.Logger;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log("Hello, World!");
+        }
+    }
+}
